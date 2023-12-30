d34928cb9a966a0b11c1d42a4ed57dd3921adfe5
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:31:55 -0700

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,19 +1,3 @@
-'''
-
-File: battleship.py
-Author: Randall Candaso
-Course/Section: CSC 120-002
-Description: The program first reads in a placement file, classifies
-each inputted ship and allocates them to their desired position on
-the 10x10 grid. Next, the program takes an inputted guess file and
-reads in each guess from the user. Indication is given if the guess
-hits a ship's position, or if it hits nothing. After all of a
-ship's position are hit, it is sunk. After all ship's have sunk,
-the program ends. Error messages are placed throughout the program
-in case the inputs given fall out of the program's parameters.
-
-'''
-
 import sys
 
 
